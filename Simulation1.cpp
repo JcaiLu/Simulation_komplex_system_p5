3a3e2080ed67e2557a6cd0d54136951dc09d85e9
Jincai Cheng <jincai.cheng@tu-braunschweig.de>
2022-03-04 00:06:41 +0100

    Chart is on

--- Simulation1.cpp
+++ Simulation1.cpp
@@ -14,7 +14,8 @@
 
 
 class TUser : public TPlan{
-
+	bool k0,k1,k2;
+	double x,phi ;
 public:
 
     int getMW,getMH;
@@ -29,26 +30,33 @@ public:
         Feld.FeldInit(getMW,getMH);
         Reset();
 		DrawInit();
+		ChartInit();
+
+		////////Test
 		Feld.InitObject();
 		Feld.CreatObject(0,0,1);
 		Feld.CreatObject(1,1,2);
-
+		////////Test
 	}
 
 	void Reset(){
         SetPen(Schwarz);SetBrush(Weiss); SetTextSize(30);               // adjust the size and color of the word
 		Text((int)(GetMaxW()*0.25),(int)(GetMaxH()*0.05),"Feld of Sea");
+		ChartReset();
 	}
 
 	void Run(){
 		DrawInit();
 		FeldDraw(Feld);
+		///////Testen
 		Feld.MoveObject(Feld.GetObject(0,0),d);
-
+		ChartRun();
 		CallRun = false;
 
 	}
-
+//====================================================================
+//Object_Draw, Feld_Draw
+//====================================================================
 	void DrawInit(){
 		SetBrush(TColor(RGB(0,255,255)));
 		for(int y = 0; y < Feld.h; y++  ){     //Zeilen
@@ -82,6 +90,59 @@ public:
 		 }
 
 
+	 }
+//====================================================================
+//Chart
+//====================================================================
+	 void ChartInit(){
+		Plot0->Size(GetMaxW()*0.55,GetMaxH()*0.08,700,900);
+        Plot0->Achse(0,"Time in Days [d]");
+        Plot0->Achse(0,0.0,100);
+        Plot0->Achse(1,0,1000);
+        Plot0->Achse(1,"Number of Fish [n]");
+        Plot0->Kurve0->SetPen(Hellrot,3);
+        Plot0->Kurve1->SetPen(Blau,3);
+        Plot0->Kurve2->SetPen(Schwarz,3);
+        InsertTaste(0,"Shark");
+		InsertTaste(1,"Salmon");
+		InsertTaste(2,"Small Fish");
+	 }
+
+	 void ChartReset(){
+		 SetPen(Schwarz);SetBrush(Weiss); SetTextSize(30);               // adjust the size and color of the word
+		 Text((int)(GetMaxW()*0.25),(int)(GetMaxH()*0.05),"Feld of Sea");
+		 Text((int)(GetMaxW()*0.73),(int)(GetMaxH()*0.05),"Changes of Fish");
+		 Plot0->Reset();
+		 k0 = true ;
+		 k1 = true ;
+		 k2 = true ;
+		 x = 0.0;
+		 phi = 0.0;
+	 }
+
+	 void ChartRun(){
+		 x=0.0;
+		 Plot0->Reset();
+		 while(x<100){
+		 if(k0) Plot0->Kurve0->LineTo(x,500*sin(x-phi));
+		 if(k1) Plot0->Kurve1->LineTo(x,1000*cos(x+phi));
+		 if(k2) Plot0->Kurve2->LineTo(x,700*sin(x-phi));
+		 x+=2; }
+		 phi+=1;
+	 }
+//====================================================================
+//Tasten
+//====================================================================
+	 void RunTaste0(){
+		 k0 = !k0 ;
+	 }
+
+	 void RunTaste1(){
+		 k1 = !k1 ;
+	 }
+
+	 void RunTaste2(){
+		 k2 = !k2 ;
 	 }
 
  };
--- others/Simulation1.cpp
+++ others/Simulation1.cpp
@@ -7,77 +7,128 @@
 #include <vcl.h>
 #pragma hdrstop
 #include "Plan.h"
-
-class Tabelle{
-    PlanString string;
-
-
-public:
-    Tabelle(){
-    string = "";}
-
-    void Set (PlanString s){
-        string = s;}
-
-        PlanString Get(){
-        return string;}
-};
+#include "ObjectGroup.h"
+#include "SeaFeld.h"
 
 
-class TUser :public TPlan {
-Tabelle a;
-Tabelle b;
-Tabelle c;
-Tabelle d;
-Tabelle e;
-Tabelle f;
-Tabelle g;
-Tabelle h;
-Tabelle i;
+
+
+class TUser : public TPlan{
+    bool k0,k1,k2;
+    double x,phi ;
 public:
 
+    int getMW,getMH;
+	int xxxxx;
+    SeaFeld Feld;
+	static const char d = 'd';
+    
     void Init(){
 
-/*      a.Set("a");
-        b.Set("b") ;
-        c.Set("c") ;
-        d.Set("d") ;
-        e.Set("e") ;
-        f.Set("f") ;
-        g.Set("g") ;
-        h.Set("h") ;
-        i.Set("i") ;            */
+        getMW = GetMaxW();
+        getMH = GetMaxH();
+        Feld.FeldInit(getMW,getMH);
         Reset();
- }
-    void Reset(){
-        Text((int)(GetMaxW()*0.7),(int)(GetMaxH()*0.05),"Menge jedes Fisches");
-        Rectangle (1050,100,500,400);
-        Text(1220,130,"Hai");
-        Text(1300,130,"Salmon");
-        Text(1410,130,"Kleinfisch");
-        Text(1060,210,"Lebendig");
-        Text(1060,310,"Tot");
-        Text(1060,420,"Insgesamt");
-        MoveTo (1190,100);
-        LineTo (1190,500);
-        MoveTo (1290,100);
-        LineTo (1290,500);
-        MoveTo (1390,100);
-        LineTo (1390,500);
-        MoveTo (1050,190);
-        LineTo (1550,190);
-        MoveTo (1050,290);
-        LineTo (1550,290);
-        MoveTo (1050,390);
-        LineTo (1550,390);
- /*     Text((int)(GetMaxW()*0.25),(int)(GetMaxH()*0.05),g.Get());
-        Text((int)(GetMaxW()*0.25),(int)(GetMaxH()*0.05),h.Get());
-        Text((int)(GetMaxW()*0.25),(int)(GetMaxH()*0.05),i.Get());   */
-         }
-  };
+		DrawInit();
+		Feld.InitObject();
+		Feld.CreatObject(0,0,1);
+		Feld.CreatObject(1,1,2);
+        Plot0->Size(GetMaxW()*0.55,GetMaxH()*0.08,700,900);
+        Plot0->Achse(0,"Time in Days [d]");
+        Plot0->Achse(0,0.0,100);
+        Plot0->Achse(1,0,1000);
+        Plot0->Achse(1,"Number of Fish [n]");
+        Plot0->Kurve0->SetPen(Hellrot,3);
+        Plot0->Kurve1->SetPen(Blau,3);
+        Plot0->Kurve2->SetPen(Schwarz,3);
+        InsertTaste(0,"Shark");
+        InsertTaste(1,"Salmon");
+        InsertTaste(2,"Small Fish");
+
+	}
+
+
+    	void Run(){
+		DrawInit();
+		FeldDraw(Feld);
+		Feld.MoveObject(Feld.GetObject(0,0),d);
+
+                x=0.0;
+                Plot0->Reset();
+                while(x<100){
+                if(k0) Plot0->Kurve0->LineTo(x,500*sin(x-phi));
+                if(k1) Plot0->Kurve1->LineTo(x,1000*cos(x+phi));
+                if(k2) Plot0->Kurve2->LineTo(x,700*sin(x-phi));
+                x+=2; }
+                phi+=1;
+
+                CallRun = true;
+
+
+	}
+
+	void Reset(){
+        SetPen(Schwarz);SetBrush(Weiss); SetTextSize(30);               // adjust the size and color of the word
+		Text((int)(GetMaxW()*0.25),(int)(GetMaxH()*0.05),"Feld of Sea");
+                Text((int)(GetMaxW()*0.73),(int)(GetMaxH()*0.05),"Changes of Fish");
+                Plot0->Reset();
+                k0 = true ;
+                k1 = true ;
+                k2 = true ;
+                x = 0.0;
+                phi = 0.0;
+	}
+        void RunTaste0(){
+             k0 = !k0 ; }
+        void RunTaste1(){
+             k1 = !k1 ; }
+        void RunTaste2(){
+             k2 = !k2 ; }
+
+
+	void DrawInit(){
+		SetBrush(TColor(RGB(0,255,255)));
+		for(int y = 0; y < Feld.h; y++  ){     //Zeilen
+			for(int x = 0; x < Feld.w; x++){          //Spalten
+			int colorDiff =255-(float)y/(float)Feld.h*100;
+			SetBrushColor(TColor(RGB(0,colorDiff,255)));
+			//SetPenColor(Schwarz);
+			SetPen(TColor(RGB(0,colorDiff,255)));
+				Rectangle(Feld.startScreenToLeft+x*Feld.pixelW,Feld.startScreenToTop+y*Feld.pixelW,Feld.pixelW,Feld.pixelW);
+			}
+		}
+
+	}
+
+	void ObjectDraw(SeaFeld playFeld,Object t){
+		for(int j = 0;j<playFeld.pixelW;j++){
+			for(int i = 0;i <playFeld.pixelW;i++){
+				if(t.shape[j][i] != 0){
+					SetPixel(t.xInPixel+i,t.yInPixel+j,Schwarz);
+				}
+			}
+		}
+}
+	 void FeldDraw(SeaFeld playFeld){
+		 for(int j = 0;j<playFeld.h;j++){
+			 for(int i = 0;i<playFeld.w;i++){
+				 if(Feld.GetObject(i,j).code != 0 ){
+					 ObjectDraw(Feld,Feld.GetObject(i,j));
+				 }
+			 }
+		 }
+
+
+	 }
+
+ };
+
+
 
 //====================================================================
 #pragma argsused
 int main(int argc, char* argv[]){ TUser a; a.Execute(); return 0; }
 //__________________________________________________________Ost08_____
+
+
 
