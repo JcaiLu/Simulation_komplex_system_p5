96a23f994a2cc83051061b49bd6dec5928340523
Jincai Cheng <jincai.cheng@tu-braunschweig.de>
2022-03-11 10:50:20 +0100

    Exposition of the Function added

--- ObjectGroup.cpp
+++ ObjectGroup.cpp
@@ -7,44 +7,113 @@
 #include "ObjectGroup.h"
 
 //---------------------------------------------------------------------------
+//Initialization the object according to the given code(c) or the
+//coordinate(px,py) and given code(c)
+//---------------------------------------------------------------------------
 
-
-
-
-int Object::GetX(){
-   return X;
+Object::Object(int c){
+	this->code = c;
+	this->GetValueRange();
+	this->xInPixel = 0;
+	this->yInPixel = 0;
+	this->X = 0;
+	this->Y = 0;
+	this->fishScale = 0;
+	memset(this->shape,0,sizeof(this->shape)) ;
 }
 
-int Object::GetY(){
-   return Y;
-}
-void Object::ReadX(int value){
-   this->X = value;
-}
+Object::Object(int px,int py,int c){
+	this->X = px;
+	this->Y = py;
+	this->code = c;
+	this->GetValueRange();
+	this->xInPixel = 0;
+	this->yInPixel = 0;
+	this->fishScale = 0;
+	memset(this->shape,0,sizeof(this->shape)) ;
 
-void Object::ReadY(int value){
-   this->Y = value;
-}
-
-
-
-bool Object::IfFull(){
-        if(this->degreeFull>0.8){
-            return true;
-        }else{
-            return false;
-        }
 }
 
+//---------------------------------------------------------------------------
+//The following Function is used to change the relevant range of the object.
+//---------------------------------------------------------------------------
 
-bool Object::IfLive(){
-		if(this->degreeLife>0.8){
-            return true;
-        }else{
-			return false;
-        }
+void Object::GetValueRange(){         //Set corresponding Value of the range
+	switch(this->code){
+		case 0:this->detectRange = 0;
+			   this->warnRange = 0;
+			   this->possibleRange = 0;
+			   this->huntRange = 0;
+			   break;
+		case 1:this->detectRange = 1;
+			   this->warnRange = 2;
+			   this->possibleFeld = 1;
+			   this->huntRange = 0;
+			   break;
+		case 2:this->detectRange = 6;
+			   this->warnRange = 6;
+			   this->possibleFeld = 2;
+			   this->huntRange = 1;
+			   break;
+		case 3:this->detectRange = 14;
+			   this->warnRange = 12;
+			   this->possibleFeld = 3;
+			   this->huntRange = 2;
+			   break;
+	}
 }
 
+void Object::GetDetectFeld(){           //detecting Range
+
+	this->GetValueRange();
+	int Range = this->detectRange;
+	this->detectFeld[0] = this->X - Range;
+	this->detectFeld[1] = this->X + Range;
+	this->detectFeld[2] = this->Y - Range;
+	this->detectFeld[3] = this->Y + Range;
+	this->BoundaryTreatment(this->detectFeld);
+
+}
+
+void Object::GetWarnFeld(){            //Warning Range
+
+	this->GetValueRange();
+	int Range = this->warnRange;
+	this->warnFeld[0] = this->X - Range;
+	this->warnFeld[1] = this->X + Range;
+	this->warnFeld[2] = this->Y - Range;
+	this->warnFeld[3] = this->Y + Range;
+	this->BoundaryTreatment(this->warnFeld);
+
+}
+
+void Object::GetHuntFeld(){            //Hunting Range
+
+	this->GetValueRange();
+	int Range = this->huntRange;
+	this->huntFeld[0] = this->X - Range;
+	this->huntFeld[1] = this->X + Range;
+	this->huntFeld[2] = this->Y - Range;
+	this->huntFeld[3] = this->Y + Range;
+	this->BoundaryTreatment(this->huntFeld);
+
+}
+
+void Object::GetPossibleFeld(){        //Possible Range
+
+	this->GetValueRange();
+	int Range = this->possibleRange;
+	this->possibleFeld[0] = this->X - Range;
+	this->possibleFeld[1] = this->X + Range;
+	this->possibleFeld[2] = this->Y - Range;
+	this->possibleFeld[3] = this->Y + Range;
+	this->BoundaryTreatment(this->possibleFeld);
+}
+
+//---------------------------------------------------------------------------
+//The following Function is used to copy a basic object to this
+//Input Type: (int)code
+//---------------------------------------------------------------------------
 
 void Object::CopyOf(int t){
 	this->code = t;
@@ -83,7 +152,9 @@ void Object::CopyOf(int t){
 				 break;
 	}
 }
-
+//---------------------------------------------------------------------------
+//The following Function is  update the Shape of this according to this code
+//---------------------------------------------------------------------------
 
 void Object::CodeShapeUpdate(){
 	switch (this->code){
@@ -122,103 +193,10 @@ void Object::CodeShapeUpdate(){
 	}
 }
 
-Object::Object(int px,int py,int c){
-	this->X = px;
-	this->Y = py;
-	this->code = c;
-	this->GetValueRange();
-	this->xInPixel = 0;
-	this->yInPixel = 0;
-	this->fishScale = 0;
-	memset(this->shape,0,sizeof(this->shape)) ;
-
-}
-
-Object::Object(int c){
-	this->code = c;
-	this->GetValueRange();
-	this->xInPixel = 0;
-	this->yInPixel = 0;
-	this->X = 0;
-	this->Y = 0;
-	this->fishScale = 0;
-	memset(this->shape,0,sizeof(this->shape)) ;
-}
-////////////////////////////////////////////////////////////////
-void Object::GetValueRange(){               	///ÐÞ¸ÄÌ½²â·¶Î§
-	switch(this->code){
-		case 0:this->detectRange = 0;
-			   this->warnRange = 0;
-			   break;
-		case 1:this->detectRange = 1;
-			   this->warnRange = 2;
-			   break;
-		case 2:this->detectRange = 6;
-			   this->warnRange = 6;
-			   break;
-		case 3:this->detectRange = 14;
-			   this->warnRange = 12;
-			   break;
-	}
-
-}
-//////////////////
-//detectFeld[4] = {A,B,C,D}
-//(A,C)..........
-//...............
-//..............
-//...........(B,D)
-
-void Object::GetDetectFeld(){           //detecting Range
-	 this->GetValueRange();
-	 int Range = this->detectRange;
-	 this->detectFeld[0] = this->X - Range;
-	 this->detectFeld[1] = this->X + Range;
-	 this->detectFeld[2] = this->Y - Range;
-	 this->detectFeld[3] = this->Y + Range;
-	 this->BoundaryTreatment(this->detectFeld);
-}
-
-void Object::GetWarnFeld(){            //Warning Range
-	 this->GetValueRange();
-	 int Range = this->warnRange;
-	 this->warnFeld[0] = this->X - Range;
-	 this->warnFeld[1] = this->X + Range;
-	 this->warnFeld[2] = this->Y - Range;
-	 this->warnFeld[3] = this->Y + Range;
-	 this->BoundaryTreatment(this->warnFeld);
-}
-
-
-void Object::GetHuntFeld(){            //Hunting Range
-	 int huntRange;
-	 if(this->code == 3){
-		 huntRange =2;
-	 }else if(this->code == 2){
-		 huntRange =1;
-	 }
-	 this->huntFeld[0] = this->X - huntRange;
-	 this->huntFeld[1] = this->X + huntRange;
-	 this->huntFeld[2] = this->Y - huntRange;
-	 this->huntFeld[3] = this->Y + huntRange;
-	 this->BoundaryTreatment(this->huntFeld);
-
-}
-
-void Object::GetPossibleFeld(){        //Possible Range
-	 int possibleRange;
-	 if(this->code == 2){
-		 this->huntRange = 2;
-	 }else if(this->code == 1){
-		 this->huntRange =1;
-	 }
-	 this->possibleFeld[0] = this->X - possibleRange;
-	 this->possibleFeld[1] = this->X + possibleRange;
-	 this->possibleFeld[2] = this->Y - possibleRange;
-	 this->possibleFeld[3] = this->Y + possibleRange;
-	 this->BoundaryTreatment(this->possibleFeld);
-}
-
+//---------------------------------------------------------------------------
+//The following Function is  to deal with the problem of the Boundary
+//Input Type: array
+//---------------------------------------------------------------------------
 
 void Object::BoundaryTreatment(int arr[]){      // this is used to avoid the object run out of SeaFeld
 	SeaFeld *a;
@@ -243,30 +221,49 @@ void Object::BoundaryTreatment(int arr[]){      // this is used to avoid the obj
 
 
 }
+//---------------------------------------------------------------------------
+//
+//
+//---------------------------------------------------------------------------
+
+int Object::GetX(){
+   return X;
+}
+
+int Object::GetY(){
+   return Y;
+}
+void Object::ReadX(int value){
+   this->X = value;
+}
+
+void Object::ReadY(int value){
+   this->Y = value;
+}
 
 
 
+bool Object::IfFull(){
+		if(this->degreeFull>0.8){
+			return true;
+		}else{
+			return false;
+		}
+}
 
 
+bool Object::IfLive(){
+		if(this->degreeLife>0.8){
+			return true;
+		}else{
+			return false;
+		}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//---------------------------------------------------------------------------
+//The following constants are used to describe the shape of the code
+//Input Type: array
+//---------------------------------------------------------------------------
 
 
 const int Object::shapeSea[10][10]={
--- SeaFeld.cpp
+++ SeaFeld.cpp
@@ -11,6 +11,58 @@
 
 #pragma package(smart_init)
 
+//---------------------------------------------------------------------------
+//The Function FeldInit() is used to initialize a Object with Class SeaFeld.
+//The following variable values are given.
+//    1) pixelW;    the heigh and the width of each element in SeaFeld.
+//    2) startScreenToLeft, startScreenToRight, startScreenToTop,startScreenToBottom
+//(This is ez to understand :) )
+//    3) playScreenW,playScreenH;    This describes how many rows and columns
+//there are in the matrix.
+//Input Type: (int)GetMaxW(),(int)GetMaxH()
+//---------------------------------------------------------------------------
+
+void SeaFeld::FeldInit(int &getW,int &getH){
+	this->pixelW = 10;
+	this->startScreenToLeft = (int)(getW*0.05);
+	this->startScreenToRight = (int)(getW*0.5);
+	this->startScreenToTop =(int)(getH*0.1);
+	this->startScreenToBottom =(int)(getH*0.05);
+	this->playScreenW = (int)((getW - this->startScreenToLeft - this->startScreenToRight)/this->pixelW);
+	this->playScreenH = (int)((getH - this->startScreenToTop - this->startScreenToBottom)/this->pixelW);
+	this->w = playScreenW;
+	this->h = playScreenH;
+
+}
+
+//---------------------------------------------------------------------------
+//The Function InitObject() is used to initialize the object in the matrix
+//
+//Beside all the object is initialized to the "Sea",the following variable
+//values are also given.
+//    1) x,y;  the coordiate of this object
+//---------------------------------------------------------------------------
+
+void SeaFeld::InitObject(){
+	Object initObject(0);
+	for(int j = 0; j < this->h; j++){          //initiazation the position of each element
+		for(int i = 0; i < this->w; i++){
+			//int k = this->XYIntoNum(i,j);
+			initObject.xInPixel = this->startScreenToLeft+i*this->pixelW;
+			initObject.yInPixel = this->startScreenToTop+j*this->pixelW;
+			initObject.X = i;
+			initObject.Y = j;
+			this->matrix.push_back(initObject);
+		}
+	}
+
+}
+
+//---------------------------------------------------------------------------
+//The Function Update() is the main and the most important program. It descirbes
+//the flow chart of our Rules.
+//
+//---------------------------------------------------------------------------
 void SeaFeld::Update(){
 
 	for(int i = 0;i < this->matrix.size();i++){
@@ -27,19 +79,12 @@ void SeaFeld::Update(){
 
 }
 
-void SeaFeld::FeldInit(int &getW,int &getH){
-	this->pixelW = 10;
-    this->startScreenToLeft = (int)(getW*0.05);
-	this->startScreenToRight = (int)(getW*0.5);
-    this->startScreenToTop =(int)(getH*0.1);
-	this->startScreenToBottom =(int)(getH*0.05);
-	this->playScreenW = (int)((getW - this->startScreenToLeft - this->startScreenToRight)/this->pixelW);
-	this->playScreenH = (int)((getH - this->startScreenToTop - this->startScreenToBottom)/this->pixelW);
-	this->w = playScreenW;
-	this->h = playScreenH;
-
-
-}
+//---------------------------------------------------------------------------
+//The following three Function are used to translate the X,Y to the Nummer of
+//the Vector.
+// Why?   Because this Vector is 1D, but the Matrix is 2D.
+//Function GetObject() can be used to get a object at x,y.
+//---------------------------------------------------------------------------
 
 int SeaFeld::XYIntoNum(int &x,int &y){             // transfer x,y into num of Vector
 
@@ -62,32 +107,18 @@ int SeaFeld::NumIntoY(int &num){             // transfer x,y into num of Vector
 
 }
 
-void SeaFeld::CreatObject(int m, int n,int code){  //
-	 int num = this->XYIntoNum(m,n);
-	 this->matrix[num].CopyOf(code);
-}
-
-
-
 Object SeaFeld::GetObject(int i, int j){
-    int n = 0;
+	int n = 0;
 	n = j*this->w +i;
 	return this->matrix[n];
 }
-
-void SeaFeld::InitObject(){
-	Object initObject(0);
-	for(int j = 0; j < this->h; j++){          //initiazation the position of each element
-		for(int i = 0; i < this->w; i++){
-			//int k = this->XYIntoNum(i,j);
-			initObject.xInPixel = this->startScreenToLeft+i*this->pixelW;
-			initObject.yInPixel = this->startScreenToTop+j*this->pixelW;
-			initObject.X = i;
-			initObject.Y = j;
-			this->matrix.push_back(initObject);
-		}
-	}
-
+//---------------------------------------------------------------------------
+//The following Function are used to creat or destory a object according to
+// the code in the SeaFeld.
+//---------------------------------------------------------------------------
+void SeaFeld::CreatObject(int m, int n,int code){  //
+	 int num = this->XYIntoNum(m,n);
+	 this->matrix[num].CopyOf(code);
 }
 
 void SeaFeld::DestoryObject(int m,int n){
@@ -99,6 +130,48 @@ void SeaFeld::DestoryObject(int m,int n){
 void SeaFeld::DestoryObject(int num){
 	 this->matrix[num].CopyOf(0);
 
+}
+
+//---------------------------------------------------------------------------
+//The following Function are used to realize a Movement of a object according
+//to the given direction.
+//---------------------------------------------------------------------------
+void SeaFeld::Move(const Object &object){      //Movement
+
+	 int xmin,xmax,ymin,ymax;
+	 if(object.target[1]>=0){
+
+		xmin= object.detectFeld[0];
+		xmax= object.detectFeld[1];
+		ymin= object.detectFeld[2];
+		ymax= object.detectFeld[3];
+
+		for(int j = ymin; j<=ymax;j++ ){
+			for(int i = xmin;i<=xmax;i++){
+				this->DestoryObject(i,j);
+			}
+		}
+	 }
+
+
+	 for (int i = 0; i < object.speed; i++) {
+		 switch(object.direction)
+		 {
+			  case '0' : DestoryObject(object.X,object.Y+i);
+						 break;
+
+			  case '1' : DestoryObject(object.X,object.Y-i);
+						 break;
+
+			  case '2' : DestoryObject(object.X-i,object.Y);
+						 break;
+
+			  case '3' : DestoryObject(object.X+i,object.Y);
+						 break;
+		 }
+	 }
+
+	 ////////////////////对象复制  (这个OBJECT的信息复制到新的target的坐标上
 }
 
 void SeaFeld::MoveObject(Object object,const char &c){
@@ -156,9 +229,7 @@ void SeaFeld::MoveObject(Object object,const char &c){
 
 }
 
-void SeaFeld::KeepMoveObject(Object object,const char &c){
 
-}
 
 
 bool SeaFeld::TargetLocation(const Object &object){
@@ -185,43 +256,7 @@ bool SeaFeld::TargetLocation(const Object &object){
 	 }
 }
 //////////// 需要补充
-void SeaFeld::Move(const Object &object){      //Movement
 
-	 int xmin,xmax,ymin,ymax;
-	 if(object.target[1]>=0){
-
-		xmin= object.detectFeld[0];
-		xmax= object.detectFeld[1];
-		ymin= object.detectFeld[2];
-		ymax= object.detectFeld[3];
-
-		for(int j = ymin; j<=ymax;j++ ){
-			for(int i = xmin;i<=xmax;i++){
-				this->DestoryObject(i,j);
-			}
-		}
-	 }
-
-
-	 for (int i = 0; i < object.speed; i++) {
-		 switch(object.direction)
-		 {
-			  case '0' : DestoryObject(object.X,object.Y+i);
-						 break;
-
-			  case '1' : DestoryObject(object.X,object.Y-i);
-						 break;
-
-			  case '2' : DestoryObject(object.X-i,object.Y);
-						 break;
-
-			  case '3' : DestoryObject(object.X+i,object.Y);
-						 break;
-		 }
-	 }
-
-	 ////////////////////对象复制  (这个OBJECT的信息复制到新的target的坐标上
-}
 
 ////////////需要补充
 void SeaFeld::Catch(){
@@ -232,5 +267,7 @@ void SeaFeld::RunAway(){
 
 }
 
+void SeaFeld::KeepMoveObject(Object object,const char &c){
 
+}
 
